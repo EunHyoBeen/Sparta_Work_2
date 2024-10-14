056e153f525b54591cb7b3000afeb074803de961
hyobeen12 <a55974124@gmail.com>
2024-10-14 10:21:58 +0900

    init

---
+++ Assets/Script/TopDownMovement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    private TopDownController controller;
+    private Rigidbody2D rb;
+
+    private Vector2 movementDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void move(Vector2 vector)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void FixedUpdate()
+    {
+        Applymovement(movementDirection);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+
+    private void Applymovement(Vector2 direction)
+    {
+        direction = direction * 5;
+        rb.velocity = direction;
+    }
+}
